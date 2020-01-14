f1e07e616d491f61ffa317464b2227338a971699
Adriaan van der Bergh <avanderbergh@gmail.com>
2020-01-14 12:35:20 +0100

    Added SuperLogo Module

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ExampleComponent from 'super-library'
+import { SuperLogo } from "super-library";
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <SuperLogo />
       </div>
-    )
+    );
   }
 }
