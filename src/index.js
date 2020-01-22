ed8478753bf775c04f7b568377cfbc2affee20b1
Felix Seeger <felix@super-code.de>
2020-01-22 15:36:43 +0100

    fixed index added team icon

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import WaitIcon from "./WaitIcon/WaitIcon";
 import BeratungIcon from "./BeratungIcon/BeratungIcon"
 import FinanzierungIcon from "./FinanzierungIcon/FinanzierungIcon"
 import FinanzierungIcon from "./MethodikIcon/MethodikIcon"
+import FinanzierungIcon from "./TeamIcon/TeamIcon"
 
 
-export { SuperLogo, WaitIcon, BeratungIcon, FinanzierungIcon, MethodikIcon };
+export { SuperLogo, WaitIcon, BeratungIcon, FinanzierungIcon, MethodikIcon, TeamIcon };
 
-export default { SuperLogo, WaitIcon, BeratungIcon, FinanzierungIcon, MethodikIcon };
+export default { SuperLogo, WaitIcon, BeratungIcon, FinanzierungIcon, MethodikIcon, TeamIcon };
