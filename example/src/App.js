4de4ec9f0156edb57b1c407b839bdf14428a5469
Felix Seeger <felix@super-code.de>
2020-01-22 14:00:35 +0100

    add FinanzierungIcon

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { SuperLogo, WaitIcon, BeratungIcon } from "super-library";
+import { SuperLogo, WaitIcon, BeratungIcon, FinanzierungIcon } from "super-library";
 
 export default class App extends Component {
   render() {
@@ -8,6 +8,7 @@ export default class App extends Component {
       <div>
         <WaitIcon height={256} />
         <BeratungIcon height={256} />
+        <FinanzierungIcon height={256} />        
         <SuperLogo />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import SuperLogo from "./SuperLogo/SuperLogo";
 import WaitIcon from "./WaitIcon/WaitIcon";
 import BeratungIcon from "./BeratungIcon/BeratungIcon"
+import BeratungIcon from "./FinanzierungIcon/FinanzierungIcon"
 
-export { SuperLogo, WaitIcon, BeratungIcon };
+export { SuperLogo, WaitIcon, BeratungIcon, FinanzierungIcon };
 
-export default { SuperLogo, WaitIcon, BeratungIcon };
+export default { SuperLogo, WaitIcon, BeratungIcon, FinanzierungIcon };
