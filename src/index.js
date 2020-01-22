6f6642177c206ff23c54fd5c34b9c8c8aab55510
Felix Seeger <felix@super-code.de>
2020-01-22 15:38:06 +0100

    fixed index error

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import SuperLogo from "./SuperLogo/SuperLogo";
 import WaitIcon from "./WaitIcon/WaitIcon";
 import BeratungIcon from "./BeratungIcon/BeratungIcon"
 import FinanzierungIcon from "./FinanzierungIcon/FinanzierungIcon"
-import FinanzierungIcon from "./MethodikIcon/MethodikIcon"
-import FinanzierungIcon from "./TeamIcon/TeamIcon"
+import MethodikIcon from "./MethodikIcon/MethodikIcon"
+import TeamIcon from "./TeamIcon/TeamIcon"
 
 
 export { SuperLogo, WaitIcon, BeratungIcon, FinanzierungIcon, MethodikIcon, TeamIcon };
